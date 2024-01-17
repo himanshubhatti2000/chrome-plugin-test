d8f268fbaf44b5594bb69a124217cc951a63cbb5
Himanshu Bhatti <himanshub@datakynd.com>
2024-01-17 17:04:33 +0530

    initial commit

---
+++ content.js
@@ -0,0 +1,10 @@
+(function() {
+    var button = document.createElement('button');
+    button.textContent = 'Test';
+    button.addEventListener('click', function() {
+      alert('message');
+    });
+  
+    document.body.appendChild(button);
+  })();
+  
